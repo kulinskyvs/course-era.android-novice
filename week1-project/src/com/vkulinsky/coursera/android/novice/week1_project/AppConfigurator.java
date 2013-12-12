872062bd335fa3aee93ee09daa39de12ca88c053
kulinskyvs <kulinskyvs@gmail.com>
2013-12-12 22:24:12 +0300

    Adding logcat logging messages to the application

---
+++ week1-project/src/com/vkulinsky/coursera/android/novice/week1_project/AppConfigurator.java
@@ -0,0 +1,7 @@
+package com.vkulinsky.coursera.android.novice.week1_project;
+
+public interface AppConfigurator {
+
+	public static final String COMMON_LOG_TAG = "coursera-common";
+	public static final String BANANA_LOG_TAG = "coursera-banana";
+}
--- week1-project/src/com/vkulinsky/coursera/android/novice/week1_project/MainActivity.java
+++ week1-project/src/com/vkulinsky/coursera/android/novice/week1_project/MainActivity.java
@@ -2,6 +2,7 @@ package com.vkulinsky.coursera.android.novice.week1_project;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.util.Log;
 import android.view.Menu;
 
 public class MainActivity extends Activity {
@@ -10,12 +11,16 @@ public class MainActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+
+		Log.i(AppConfigurator.COMMON_LOG_TAG, "main activity is created");
 	}
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.main, menu);
+
+		Log.e(AppConfigurator.BANANA_LOG_TAG, "Just a simple error message for test purposes");
 		return true;
 	}
 
